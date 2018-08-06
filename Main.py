b9fe55fa6c15aee937bc26eddcb3f47bb4e24482
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-08-06 13:38:35 +0200

    Added 2 buttons to EditView and changed name MainController to the Controller.

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from Controller.MainController import Controller
+from Controller.Controller import Controller
 
 if __name__ == '__main__':
     root = tk.Tk()
--- View/EditView.py
+++ View/EditView.py
@@ -1,18 +1,24 @@
 import tkinter as tk
-from tkinter import N, S, E, W
+from tkinter import N, S, E, W, Button
 
 
 class EditView(tk.Toplevel):
 
     def __init__(self, master):
         tk.Toplevel.__init__(self, master)
+        self.title('Input data')
 
         self.text_area = tk.Text(self, height=10, width=30)
         self.text_area.focus_set()
-        self.text_area.grid(sticky=N + S + E + W)
+        self.text_area.grid(columnspan=2, sticky=N + S + E + W)
+
+        self.ok_btn = Button(self, text='OK')
+        self.cancel_btn = Button(self, text='Cancel')
+
+        self.ok_btn.grid(column=0, row=1, sticky=N + S + E + W)
+        self.cancel_btn.grid(column=1, row=1, sticky=N + S + E + W)
 
         self.grab_set()
-
         # Properly expand whole grid
         col_size, row_size = self.grid_size()
         self.columnconfigure(tuple(range(col_size)), weight=1)
