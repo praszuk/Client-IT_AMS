da998850f5885a05ab2844f7e20947751e5b56bf
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-08-06 12:40:47 +0200

    EditView: text_area added auto expanding for all widgets.

--- View/EditView.py
+++ View/EditView.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import N, S, E, W
 
 
 class EditView(tk.Toplevel):
@@ -8,6 +9,11 @@ class EditView(tk.Toplevel):
 
         self.text_area = tk.Text(self, height=10, width=30)
         self.text_area.focus_set()
-        self.text_area.grid()
+        self.text_area.grid(sticky=N + S + E + W)
 
         self.grab_set()
+
+        # Properly expand whole grid
+        col_size, row_size = self.grid_size()
+        self.columnconfigure(tuple(range(col_size)), weight=1)
+        self.rowconfigure(tuple(range(row_size)), weight=1)
