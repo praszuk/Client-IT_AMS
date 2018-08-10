b8fab4e9e8a0975d330f24ed338aa0a3ef1ad42b
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-08-10 18:33:05 +0200

    Implemented APIController.
    Connecting to API and fetching hardware information to parse as asset objects.

---
+++ Controller/APIController.py
@@ -0,0 +1,99 @@
+from typing import List
+
+import requests
+
+from Model.AssetModel import AssetStatus, Asset
+
+
+class APIController:
+    """
+
+    Class responsible for connecting to API
+    Getting data and parsing as Asset objects.
+
+    This app currently is planned only to fetching data for hardware.
+    """
+
+    @staticmethod
+    def parse_hardware_data(text: str):
+        """
+
+        :param text: ids/serial numbers - input from user GUI
+        :return List[Asset]: asset objects
+        """
+
+        def set_asset(resp, _asset):
+            _asset.set_id(int(resp['id']))
+            _asset.set_name(resp['name'])
+            _asset.set_serial_number(resp['serial'])
+            _asset.set_status(AssetStatus.get_status(resp['status_label']['id'], resp['status_label']['status_meta']))
+
+        assets = []
+        ids = text.split()
+
+        for _id in ids:
+            print('-' * 15)
+            asset = Asset()
+
+            try:
+                response, index = APIController.get_data_from_api(_id)
+
+                if response is None or 'total' in response and response['total'] == 0:
+                    asset.set_id(_id)
+                    asset.set_status(AssetStatus.ASSET_NOT_FOUND)
+                    print('Not found: {}'.format(asset.get_id()))
+
+                else:
+                    # By ID (database)
+                    if index == 0:
+                        set_asset(response, asset)
+
+                    # By Serial Number
+                    elif index == 1:
+                        set_asset(response['rows'][0], asset)
+
+                    print(asset)
+
+                assets.append(asset)
+
+            except IOError:
+                asset.set_status(AssetStatus.NOT_CONNECTED)
+                print('Error! Connection problem.')
+
+            except KeyError:
+                asset.set_status(AssetStatus.STATUS_NOT_FOUND)
+                print('Error! API problem.')
+
+        return assets
+
+    @staticmethod
+    def get_data_from_api(asset_id):
+        """
+
+        :param asset_id: id or serial. It's User input form Text Area EditView (GUI).
+        :rtype: (dict, int)
+        :return: response as json or None and index as endpoint which was used 0. hardware or 1. hardware/byserial.
+
+        """
+        from Main import CONFIG
+
+        endpoints: List[str] = ['hardware/', 'hardware/byserial/']
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': CONFIG.TOKEN
+        }
+
+        for index, endpoint in enumerate(endpoints):
+            r = requests.get(CONFIG.URL + endpoint + asset_id, headers=headers).json()
+
+            if 'status' in r and r['status'] == 'error':
+                if index == 0:
+                    print('Asset with ID: {} error: {}'.format(asset_id, r['messages']))
+                elif index == 1:
+                    print('Asset with Serial: {} error: {}'.format(asset_id, r['messages']))
+                continue
+
+            return r, index
+
+        return None, -1
--- Controller/EditViewController.py
+++ Controller/EditViewController.py
@@ -1,6 +1,4 @@
-import requests
-
-from Model.AssetModel import Asset, AssetStatus
+from Controller.APIController import APIController
 from Model.EditViewModel import EditViewModel
 from View.EditView import EditView
 
@@ -31,51 +29,5 @@ class EditViewController:
         self.__edit_view.withdraw()
 
     def __input_data_changed(self, text):
-        assets = self.parse_data(text)
+        assets = APIController.parse_hardware_data(text)
         # self.root.update_tree_view(assets)
-
-    @staticmethod
-    def parse_data(text):
-        from Main import CONFIG
-
-        assets = []
-        ids = text.split()
-        for _id in ids:
-            print('-' * 15)
-            asset = Asset()
-
-            try:
-                endpoint = 'hardware/'
-                headers = {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': CONFIG.TOKEN
-                }
-
-                response = requests.get(CONFIG.URL + endpoint + _id, headers=headers).json()
-
-                if 'status' in response:
-                    if response['status'] == 'error':
-                        asset.set_id(_id)
-                        asset.set_status(AssetStatus.ASSET_NOT_FOUND)
-                        print('Not found: {}'.format(asset.get_id()))
-
-                else:
-                    asset.set_id(int(response['id']))
-                    asset.set_name(response['name'])
-                    asset.set_serial_number(response['serial'])
-                    asset.set_status(AssetStatus.get_status(response['status_label']['id'],
-                                                            response['status_label']['status_meta']))
-                    print(asset)
-
-                assets.append(asset)
-
-            except IOError:
-                asset.set_status(AssetStatus.NOT_CONNECTED)
-                print('Error! Connection problem.')
-
-            except KeyError:
-                asset.set_status(AssetStatus.STATUS_NOT_FOUND)
-                print('Error! API problem.')
-
-        return assets
