cad4f304e5dfb6541b40797990194628b7732a2d
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-08-29 16:26:54 +0200

    ProductAPIController oauth optimization.
    Using expire time of token instead generating new every time.

--- Controller/ProductAPIController.py
+++ Controller/ProductAPIController.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 
@@ -5,30 +7,40 @@ class ProductAPIController:
 
     def __init__(self):
         self.__token = None
+        self.__expire_time = time.time()
 
     def __update_token(self):
         from Main import CONFIG
 
-        endpoint = 'https://cloudsso.cisco.com/as/token.oauth2'
-        parameters = {
-            'grant_type': CONFIG.GRANT_TYPE,
-            'client_id': CONFIG.CLIENT_ID,
-            'client_secret': CONFIG.CLIENT_SECRET
-        }
-        print(parameters)
-        r = requests.post(url=endpoint, params=parameters)
-        print(r.text)
-        print(r.raw)
+        if time.time() >= self.__expire_time:
+            endpoint = 'https://cloudsso.cisco.com/as/token.oauth2'
+            parameters = {
+                'grant_type': CONFIG.GRANT_TYPE,
+                'client_id': CONFIG.CLIENT_ID,
+                'client_secret': CONFIG.CLIENT_SECRET
+            }
 
-        self.__token = r.json()['access_token']
+            try:
+                response = requests.post(url=endpoint, params=parameters).json()
+                self.__token = response['access_token']
+                self.__expire_time = time.time() + response['expires_in']
+
+            except IOError:
+                self.__token = None
+                self.__expire_time = time.time()
+                print('Cannot obtain TOKEN from: {}'.format(endpoint))
 
     def get_product_data(self, serial_number):
         self.__update_token()
 
-        endpoint = 'https://api.cisco.com/product/v1/information/serial_numbers/' + serial_number
+        endpoint = 'https://api.cisco.com/product/v1/information/serial_numbers/'
         headers = {'Authorization': 'Bearer ' + self.__token}
-        r = requests.get(endpoint, headers=headers)
+
+        r = requests.get(endpoint + serial_number, headers=headers)
+
         try:
+            print('Token: {}'.format(self.__token))
             print(r.json())
+
         except:
             pass
