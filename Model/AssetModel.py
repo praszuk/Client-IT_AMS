94efab7285b30029021e93b35d6e60dbe8b79ef2
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-09-13 14:49:10 +0200

    Added category id and name to asset model.

--- Model/AssetModel.py
+++ Model/AssetModel.py
@@ -5,7 +5,8 @@ from Model.Observable import Observable
 
 class Asset:
 
-    def __init__(self, id=-1, tag='', model_id=-1, model_name='', name='', notes='', serial_number=''):
+    def __init__(self, id=-1, tag='', model_id=-1, model_name='', category_id=-1, category_name='',
+                 name='', notes='', serial_number=''):
 
         self.id = id
         self.tag = tag
@@ -14,6 +15,8 @@ class Asset:
         self.serial_number = serial_number
         self.model_id = model_id
         self.model_name = model_name
+        self.category_id = category_id
+        self.category_name = category_name
         self.__status = Observable(None)
 
     def __str__(self):
