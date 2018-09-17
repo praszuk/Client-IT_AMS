42cf9e12d8f07b8f852988d6bb144fda41804037
Paweł Raszuk <pawel.raszuk@gmail.com>
2018-09-17 19:01:48 +0200

    Added __auto_add function and extended on select

--- Controller/MainViewController.py
+++ Controller/MainViewController.py
@@ -3,7 +3,9 @@ from sys import exit
 
 from Controller.DocumentGeneratorController import DocumentGeneratorController
 from Controller.EditViewController import EditViewController
+from Controller.StockAPIController import StockAPIController
 from Model.AssetListModel import AssetListModel
+from Model.AssetModel import AssetStatus
 from Model.CheckoutModel import CheckoutModel
 from View.MainView import MainView
 
@@ -11,13 +13,16 @@ from View.MainView import MainView
 class Controller:
     def __init__(self, root):
         self.root = root
+
         self.assets = AssetListModel()
+        self.assets_to_add = AssetListModel()
 
         self.checkout_model = CheckoutModel()
         self.main_view = MainView(root, self.assets)
 
         self.main_view.tree.bind('<<TreeviewSelect>>', self.__on_select)
 
+        self.main_view.btn_auto_add.config(command=self.__auto_add)
         self.main_view.btn_edit.config(command=self.__edit_view_launcher)
         self.main_view.btn_generate.config(command=self.__document_generator_view_launcher)
         self.main_view.btn_refresh.config(command=self.__refresh_assets)
@@ -39,10 +44,52 @@ class Controller:
         self.assets.replace(self.edit_view_controller.update_assets(serials))
 
     def __on_select(self, event):
+        self.assets_to_add.clear()
+
         selection = self.main_view.tree.selection()
         for item in selection:
             asset = self.assets.get_by_serial_number(self.main_view.tree.item(item)['values'][2])
-            # TODO check if ready to add and execute properly function
+
+            if asset.status == AssetStatus.READY_TO_ADD:
+                self.assets_to_add.add(asset)
+
+        if len(self.assets_to_add) > 0:
+            self.main_view.btn_auto_add.config(state='normal')
+        else:
+            self.main_view.btn_auto_add.config(state='disable')
+
+    def __auto_add(self):
+        for asset in self.assets_to_add:
+
+            category_id = StockAPIController.get_category_id(asset.category_name)
+            if category_id == -1:
+                category_id = StockAPIController.create_category(asset.category_name)
+
+            asset.category_id = category_id
+
+            if category_id == -1:
+                logging.error('Error. Cannot create category in auto-add process for asset: {}'.format(str(asset)))
+                continue
+
+            model_id = StockAPIController.get_model_id(asset.model_name)
+            if model_id == -1:
+                model_id = StockAPIController.create_model(asset.model_name, category_id)
+
+            asset.model_id = model_id
+
+            if model_id == -1:
+                logging.error('Error. Cannot create model in auto-add process for asset: {}'.format(str(asset)))
+                continue
+
+            asset_id = StockAPIController.create_hardware(asset, 1)
+            if asset_id == -1:
+                logging.error('Error. Cannot create asset in auto-add process for asset: {}'.format(str(asset)))
+
+            else:
+                logging.info('Created asset with id: {}, asset: {}'.format(asset_id, str(asset)))
+
+        self.main_view.btn_auto_add.config(state='disable')
+        self.__refresh_assets()
 
     @staticmethod
     def __close_app():
